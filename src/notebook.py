827727f62c7edf8ff932c9c461c1c0e68479169f
b14s210 <miguelypkr@gmail.com>
2026-02-16 16:32:47 -0500

    Prueba de Commit

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,16 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+import datetime
+
+class Note:
+
+    HIGH = 'High'
+    MEDIUM = 'Medium'
+    LOW = 'Low'
+
+    def __init__(self, code: str, tittle: str, text: str, importance: int):
+        self.code = code
+        self.tittle = tittle
+        self.text = text
+        self.importance = importance
+        creation_date = datetime.datetime.now()
+        self.tags: list[str] = []
