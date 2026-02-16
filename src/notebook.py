bea071206b7e91b63b13d1ede673ad483a74e4e5
b14s210 <miguelypkr@gmail.com>
2026-02-16 17:07:01 -0500

    def add_tag, def __str__, def generate_code con e

--- src/notebook.py
+++ src/notebook.py
@@ -16,10 +16,20 @@ class Note:
         self.tags: list[str] = []
 
     def add_tag(self, tag: str):
-        if tag not in self.tags: # Si tag no esta en tags
+        if tag not in self.tags: # Si el tag creado no esta en la lista tags
             self.tags.append(tag) # Entonces esto permite para no agregar duplicados con el append
 
-    def __str__(self):
+    def __str__(self): # Este es Str para devolver un texto que va aparecer.
         return f"Date: {self.creation_date}, {self.tittle} {self.text}"
 
+class Notebook:
 
+    def __init__(self):
+        self.notes: list[Note] = []
+
+    def _generate_code(self) -> int: # Esto significa que el codigo devuelve un numero entero con el -> str
+        existing_codes = {note.code for note in self.notes} # verificar el codigo de cada nota que esta en notas los cuales se guardan en existing_codes
+        code = 1 # Inicia con el codigo 1 al ser generado
+
+
+    def add_note(self, tittle: str, text: str, importance: str):
